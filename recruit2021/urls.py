59fd2d73f24696b537db30db2ce1038a5581121b
JunhyoungPark <byjs0725@gmail.com>
2021-03-12 16:46:51 +0900

    revert ap test3

--- recruit2021/urls.py
+++ recruit2021/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('sglioninfo/', accounts.views.SglionInfoView.as_view(), name="sglioninfo"),
     path('accounts/',include('allauth.urls')),
     path('socialregister/', accounts.views.SocialregisterView.as_view(),name="socialregister"),
-    path('interviewhome/', apply.views.ApplyHomeView.as_view(),name='interviewhome'),
+    path('interviewselect/', recruit.views.ApplyView.as_view(),name='interviewselect'),
     url(r'^traffic/', include('traffic_monitor.urls')),
 ]
 if settings.DEBUG:
