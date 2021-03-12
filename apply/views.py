452cc35e0ff91a632d7734eec173355ce8ef516d
JunhyoungPark <byjs0725@gmail.com>
2021-03-12 16:48:25 +0900

    revert apply test

--- apply/views.py
+++ apply/views.py
@@ -1,21 +1,3 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
-from django.utils import timezone
-from django.views import View
-from django.views.generic import TemplateView, ListView
-from django.contrib.auth import update_session_auth_hash
-from .models import Apply
-from .forms import ApplyForm
+from django.shortcuts import render
 
-class ApplyView(View):
-    form_class = ApplyForm
-    # initial = {'key':'value'}
-    template_name = 'recruit/intervew-home.html'
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form' : form})
-    def post(self, request):
-        form = self.form_class(request.POSTe)
-        if form.is_valid():
-            return redirect('recruit/interview-select')
-        return render(request,self.template_name, {'form':form})
+# Create your views here.
--- recruit2021/urls.py
+++ recruit2021/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
     path('sglioninfo/', accounts.views.SglionInfoView.as_view(), name="sglioninfo"),
     path('accounts/',include('allauth.urls')),
     path('socialregister/', accounts.views.SocialregisterView.as_view(),name="socialregister"),
-    path('interviewselect/', recruit.views.ApplyView.as_view(),name='interviewselect'),
     url(r'^traffic/', include('traffic_monitor.urls')),
 ]
 if settings.DEBUG:
