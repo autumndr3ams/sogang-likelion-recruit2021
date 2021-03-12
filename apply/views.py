37fefd4959b924756054ab1747fd66bd9e8892e0
JunhyoungPark <byjs0725@gmail.com>
2021-03-12 16:07:14 +0900

    reset apply

--- apply/views.py
+++ apply/views.py
@@ -7,14 +7,3 @@ from django.contrib.auth import update_session_auth_hash
 from .models import Apply
 from .forms import ApplyForm
 
-class ApplyHomeView(View):
-    form_class = ApplyForm
-    template_name = 'recruit/interview-home.html'
-    def get(self, request):
-        return render(request,'recruit/interview-home.html')
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            return redirect('recruit/interviewselect')
-        return render(request,self.template_name,{'form':form})
-
