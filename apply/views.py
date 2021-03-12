b719423c25c5cccd801e486c19a2502258f61900
JunhyoungPark <byjs0725@gmail.com>
2021-03-12 16:40:34 +0900

    revert reset

--- apply/views.py
+++ apply/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.utils import timezone
+from django.views import View
+from django.views.generic import TemplateView, ListView
+from django.contrib.auth import update_session_auth_hash
+from .models import Apply
+from .forms import ApplyForm
 
